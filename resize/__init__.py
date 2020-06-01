9ee5f665c1cfea36c77cc8e0602a6c44717bda9d
Eric <eric@ericjshin.com>
2020-06-01 01:20:57 -0700

    initial commit

---
+++ resize/__init__.py
@@ -0,0 +1,39 @@
+import click
+import pillow
+
+class resize:
+
+   def __init__(self):
+      #constructor
+      pass
+
+   def isLandscape():
+      """
+         determines if an image is in landscape
+      """
+      pass
+
+   def isImage():
+      """
+         determines if a file is an image
+      """
+      pass
+
+   def resizeLandscape():
+      """
+         will resize image in landscape context
+      """
+      pass
+
+   def resizePortrait():
+      """
+         will resize image in portrait context
+      """
+      pass
+
+   def processFolder():
+      """
+         takes a folder and creates a list of images.
+         resize each image
+      """
+      pass
