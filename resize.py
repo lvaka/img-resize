62fe8b3908c4d4f7f10643ab22391979b4fcce0d
Eric <eric@ericjshin.com>
2020-06-03 22:08:26 -0700

    MVP

--- resize.py
+++ resize.py
@@ -1,2 +1,25 @@
 import click
-from resize import resize
+from resize import Batch
+import os
+
+@click.command()
+@click.option('-l','--length', default=1200, help='Maximum length of edge of image')
+@click.option('-i','--image_format', default='JPEG', help='Image format - JPEG, TIFF, WebP')
+@click.option('-c','--compression', help='Compression Level')
+@click.option('-d','--directory', required=True, help='Directory to batch resize')
+def run_main(length, image_format, compression, directory):
+   """
+      Sets config and runs batch script
+   """
+   
+   if os.path.isdir(directory):
+      batch = Batch(length=length, image_format=image_format, compression=compression, directory=directory)
+      batch.batch()
+
+
+   else:
+      print('Not a valid directory')
+   
+
+if __name__ == '__main__':
+   run_main()
--- resize/__init__.py
+++ resize/__init__.py
@@ -1,39 +1,120 @@
 import click
-import pillow
+from PIL import Image
+import magic
+import os
 
-class resize:
+class Batch:
 
-   def __init__(self):
-      #constructor
-      pass
-
-   def isLandscape():
+   def __init__(self, **kwargs):
       """
-         determines if an image is in landscape
+         Initialize class with a dict containing 
+         key value pairs for
+         length, format, compression, and directory       
       """
-      pass
+      self.length = kwargs.get('length', 1200)
+      self.image_format = kwargs.get('image_format', 'JPEG')
+      self.compression = int(kwargs.get('compression'))
+      self.directory = kwargs.get('directory')
+      self.resizeDir = os.path.join(kwargs.get('directory'), 'resized')
+      print('Batch configurations set')
 
-   def isImage():
-      """
-         determines if a file is an image
-      """
-      pass
 
-   def resizeLandscape():
+   def processImage(self, filePath, fileName, mimeType):
+      """
+         starts image process
+      """
+      acceptedMimetypes = (
+         'image/jpeg',
+         'image/webp',
+         'image/tiff',
+         'image/bmp',
+         'image/png'
+      )
+
+      if mimeType in acceptedMimetypes:
+         """
+            If mimeType is within accepted mimeTypes,
+            Open image into memory
+            Get Img Dimensions
+            Check if image is landscape
+            Resize based on image orientation
+         """
+         img = Image.open(filePath)
+         imgW, imgH = img.size
+         isLandscape = imgW > imgH
+         resizedImg = None
+
+         print('Resizing ', fileName)
+
+         if isLandscape:
+            resizedImg = self.resizeLandscape(img, imgW, imgH)
+
+         else:
+            resizedImg = self.resizePortrait(img, imgW, imgH)
+
+         if resizedImg:
+            """
+               If resized image is returned and is not falsey,
+               generate new filepath, save, then close out files
+            """
+            
+            name, extension = os.path.splitext(fileName)
+            
+            if self.image_format is 'TIFF':
+               newFileName = name + '.tiff'
+               resizeFilePath = os.path.join(self.resizeDir, newFileName)
+
+               resizedImg.save(resizeFilePath, format="TIFF", compression="tiff_jpeg",quality=self.compression)
+
+            elif self.image_format is 'WebP':
+               newFileName = name + '.webp'
+               resizeFilePath = os.path.join(self.resizeDir, newFileName)
+
+               resizedImg.save(resizeFilePath, format="WebP", quality=self.compression)
+
+            else:
+               newFileName = name + '.jpg'
+               resizeFilePath = os.path.join(self.resizeDir, newFileName)
+
+               resizedImg.save(resizeFilePath, format="JPEG", quality=self.compression)
+
+            resizedImg.close()
+
+         img.close()
+
+
+   def resizeLandscape(self, img, imgW, imgH):
       """
          will resize image in landscape context
       """
-      pass
+      dimensions = (self.length, int(self.length * imgH/imgW))
+      resizeImg = img.resize(dimensions)
+      return resizeImg
+      
 
-   def resizePortrait():
+   def resizePortrait(self, img, imgW, imgH):
       """
          will resize image in portrait context
       """
-      pass
+      dimensions = (int(self.length * imgW/imgH), self.length)
+      resizeImg = img.resize(dimensions)
+      return resizeImg
 
-   def processFolder():
+   def batch(self):
       """
          takes a folder and creates a list of images.
          resize each image
       """
-      pass
+      print('Run Batch now!')
+      
+      dirList = os.listdir(self.directory)
+      if not os.path.exists(self.resizeDir):
+         os.makedirs(self.resizeDir)
+         print('Creating resize folder')
+
+      for listing in dirList:
+         listingPath = os.path.join(self.directory, listing)
+         isFile = os.path.isfile(listingPath)
+         if isFile:
+            mimeType = magic.from_file(listingPath, mime=True)
+            self.processImage(listingPath, listing, mimeType)
